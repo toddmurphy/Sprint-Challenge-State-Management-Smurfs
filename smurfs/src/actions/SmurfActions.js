6e668c59c2f5e6ec6d81aa7807c081611185ff40
toddmurphy <toddmurphy93@gmail.com>
2019-12-13 14:04:02 -0400

    setting up actions structure and getSmurfData

---
+++ smurfs/src/actions/SmurfActions.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+//Action Types --> go here ↓
+export const FETCH_SMURF_START = 'FETCH_SMURF_START';
+export const FETCH_SMURF_SUCCESS = 'FETCH_SMURF_SUCCESS';
+export const FETCH_SMURF_FAILURE = 'FETCH_SMURF_FAILURE';
+
+
+//Action Creators (async action creator) -->  go here ↓
+// get smurf data from localhost api --> http://localhost:3333/smurfs
+
+const getSmurfData = () => dispatch => {
+    console.log('in getSmurf data')
+    dispatch({type: FETCH_SMURF_START});
+    axios.get('http://localhost:3333/smurfs')
+        .then(response => {
+            console.log(response)
+            dispatch({type: FETCH_SMURF_SUCCESS, payload: response})
+        });
+        .catch(error => {
+            dispatch({type: FETCH_SMURF_FAILURE, payload: error.response})
+        });
+}
+
+export default getSmurfData;
--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -7,10 +7,6 @@ import SmurfList from "./SmurfList";
 const App = () => {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
         <SmurfForm />
         <SmurfList />
       </div>
